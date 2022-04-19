c1b284a9f78c2fb961bdceb045cdee677f3cb902
f1nkejak <jakob.finkenzeller@hfg.design>
2022-04-19 22:07:31 +0200

    created the vue app

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'A sock is a garment for the foot and lower part of the leg, typically knitted from wool, cotton, or nylon.'
+        }
+    }
+})
